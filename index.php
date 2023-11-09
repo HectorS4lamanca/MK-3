018fa33b25825b7a33b699c2b729a382bc9f54b1
AzfarMuiz <139884001+HectorS4lamanca@users.noreply.github.com>
2023-11-09 15:45:12 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,83 +1,38 @@
+<?php
+require '../conn.php';
+if(!isset($_SESSION['idpengguna'])) header('location: ../');
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Login</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            margin: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            max-width: 400px;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 5px;
-            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-        }
-
-        h1 {
-            text-align: center;
-            color: #333;
-        }
-
-        h3 {
-            text-align: center;
-            color: #555;
-        }
-
-        form {
-            padding: 20px;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 10px;
-            color: #333;
-            font-weight: bold;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        button[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            background-color: #007bff;
-            border: none;
-            color: #fff;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-
-        button[type="submit"]:hover {
-            background-color: #0056b3;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../admin/style.css">
+    <title>i-DaftarElektrik</title>
 </head>
 <body>
-    <div class="container">
-        <h1>Sistem Pendaftaran Peralatan Elektrik</h1>
-        <h3>Log Masuk</h3>
-        <form action="login.php" method="post">
-            <label for="idpengguna">ID:</label>
-            <input type="text" name="idpengguna" id="idpengguna" required><br><br>
-            <label for="kata">Kata Laluan:</label>
-            <input type="password" name="kata" id="kata" required><br><br>
-            <button type="submit">MASUK</button>
-        </form>
-    </div>
+    <header>
+        <table>
+            <tr>    
+                <td>i-DaftarElektrik&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                    <a href="index.php?menu=home" class="menu-button">HOME</a>
+                    <a href="index.php?menu=pelajar" class="menu-button">WARDEN</a>
+                    <a href="index.php?menu=peralatan" class="menu-button">PERALATAN</a>
+                    <a href="index.php?menu=pass" class="menu-button">RESET PASSWORD</a>
+                    <a href="../logout.php" class="menu-button">LOGOUT</a>
+                </td>
+            </tr>
+        </table>
+    </header>
+
+    <?php
+    $menu = 'home'; 
+    if(isset($_GET['menu'])){
+        $menu=$_GET['menu'];
+    }
+    include "$menu.php";
+    ?>
 </body>
 </html>
